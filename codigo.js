a0b55a91f7c942bd25ca9f357167d6fdfb187635
EstebanDBH1 <120125487+EstebanDBH1@users.noreply.github.com>
2023-02-15 10:18:15 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,78 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.container__header', {
+
+    duration: 4000,
+    origin: 'bottom',
+    distance: '-100px',
+})
+
+sr.reveal('.imagenes', {
+
+    duration: 5000,
+    origin: 'top',
+    distance: '-100px',
+})
+
+
+
+
+sr.reveal('section', {
+
+    duration: 6000,
+    origin: 'top',
+    distance: '-100px',
+})
+
+
+sr.reveal('.title-perfo', {
+
+    duration: 6000,
+    origin: 'left',
+    distance: '-100px',
+})
+
+
+sr.reveal('.festival', {
+
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px',
+})
+
+
+sr.reveal('.p-img1', {
+
+    duration: 4000,
+    origin: 'right',
+    distance: '-100px',
+})
+
+
+sr.reveal('.p-img2', {
+
+    duration: 5000,
+    origin: 'bottom',
+    distance: '-100px',
+})
+
+sr.reveal('.p-img3', {
+
+    duration: 6000,
+    origin: 'top',
+    distance: '-100px',
+})
+
+sr.reveal('.p-img4', {
+
+    duration: 7000,
+    origin: 'left',
+    distance: '-100px',
+})
+
+sr.reveal('.tickets', {
+
+    duration: 5000,
+    origin: 'top',
+    distance: '-100px',
+})
